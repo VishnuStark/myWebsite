12e15dda990bdeebcd9c229be3f1f751882d3b72
VishnuStark <70432618+VishnuStark@users.noreply.github.com>
2020-08-29 15:17:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/css.css">
+	<link rel = "icon" href = "img/iconn.jpg" type = "image/x-icon">
+	<title>The Review</title>
+</head>
+<body>
+<div class="header">
+	<div class="main">
+		<div class="logo">
+			<a href="index.php"><img src="img/logo.jpg"></a>
+		</div>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="Specs.php">Specs</a></li>
+			<li><a href="Review.php">Review</a></li>
+			<li><a href="FindPhone.php">Find Phone</a></li>
+			<li><a href="Compare.php">Compare</a></li>
+			<li><a href="News.php">News</a></li>
+			<li><a href="Contact.php">Contact</a></li>
+			<li><button class="btn btnn">Vishnu</button></li>
+		</ul>
+	</div>
+</div>
+</body>
+</html>
